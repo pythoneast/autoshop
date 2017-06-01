60626b7e359e0c7d20bd46fe099e57be2a84bc3a
Ali <mrpalitaev@gmail.com>
2017-06-01 22:30:54 +0600

    cart

--- autoshop/static/js/cart.js
+++ autoshop/static/js/cart.js
@@ -5,6 +5,7 @@
 /* Корзина */
 
 function checkCart() {
+    // проверяю наличие корзины в localStorage
     if (localStorage.getItem('cart') != null) {
         var cart = JSON.parse(localStorage.getItem('cart'));
         var urlParams = '?' +
@@ -22,14 +23,79 @@ function checkCart() {
                     return false;
                 }
 
-                var out='';
-                for (var i = 0; i < products.length; i++ ){
-                    out += createDiv(products[i], cart);
-                }
-                $('#my-cart').html(out);
-                $('#btn-checkout').show();
+                renderCart(products, cart);
             }
+
+
+
         });
+    }
+}
+
+function renderCart(products, cart) {
+    var out = '';
+    for (var i = 0; i < products.length; i++) {
+        out += createDiv(products[i], cart);
+    }
+    $('#my-cart').html(out);
+    if (products.length < 1) {
+        $('#btn-checkout').hide();
+        $('footer.jumbotron2').addClass('nujno');
+    }  else {
+        if (products.length == 1) {
+            $('footer.jumbotron2').addClass('nujno');
+            $('#btn-checkout').show();
+            $('.plus').on('click', function () {
+                var articul = $(this).attr('data-art');
+                cart[articul] = cart[articul] + 1;
+                saveCartToLS(cart); // сохраняю корзину в localStorage
+                renderCart(products, cart);
+            });
+            $('.minus').on('click', function () {
+                var articul = $(this).attr('data-art');
+                if (cart[articul] > 1) {
+                    cart[articul] = cart[articul] - 1;
+                    saveCartToLS(cart); // сохраняю корзину в localStorage
+                    renderCart(products, cart);
+                }
+            });
+            $('.delete').on('click', function () {
+                var articul = $(this).attr('data-art');
+                delete cart[articul];
+                products = products.filter(function (x) {
+                    return x.id !== +articul;
+                });
+                saveCartToLS(cart); // сохраняю корзину в localStorage
+                renderCart(products, cart);
+            });
+        }
+        else {
+            $('footer.jumbotron2').removeClass('nujno');
+            $('#btn-checkout').show();
+            $('.plus').on('click', function () {
+                var articul = $(this).attr('data-art');
+                cart[articul] = cart[articul] + 1;
+                saveCartToLS(cart); // сохраняю корзину в localStorage
+                renderCart(products, cart);
+            });
+            $('.minus').on('click', function () {
+                var articul = $(this).attr('data-art');
+                if (cart[articul] > 1) {
+                    cart[articul] = cart[articul] - 1;
+                    saveCartToLS(cart); // сохраняю корзину в localStorage
+                    renderCart(products, cart);
+                }
+            });
+            $('.delete').on('click', function () {
+                var articul = $(this).attr('data-art');
+                delete cart[articul];
+                products = products.filter(function (x) {
+                    return x.id !== +articul;
+                });
+                saveCartToLS(cart); // сохраняю корзину в localStorage
+                renderCart(products, cart);
+            });
+        }
     }
 }
 
@@ -41,10 +107,14 @@ function createDiv(product, cart) {
                         +'<div class="col-xs-12 col-sm-4">'
                             +'<img src="/media/'+ product.picture + '" style="margin-top: 20px; margin-bottom: 20px; max-height: 100px; width:auto"/>'
                         +'</div>'
-                        +'<div class="col-xs-12 col-sm-4 opisanie" style="border: none"><p style="margin-top: 30px"><b>'+ product.title +'</b></p></div>'
+                        +'<div class="col-xs-12 col-sm-4 opisanie" style="border: none">'
+                        +'<p style="margin-top: 30px"><b>'+ product.title +'</b>'
+                        +'</p>'
+                        +'</div>'
                         +'<div class="col-xs-12 col-sm-4" style="text-align: center; margin-top: 48px; font-size: 20px;"><b>'
                             + product.price + ' ' + 'x' + ' ' + cart[product.id] + ' ' + '=' + ' ' + product.price*cart[product.id]
-                            +'</b><button class="btn btn-delete btn-danger" style="position: absolute; right: -30px; top: -40px ">x</button>'
+                            +'<br><button data-art="'+ product.id +'" style="margin-left: 20px; margin-right: 5px;" class="minus btn btn-info">-</button><b>'+ cart[product.id] +'</b><button data-art="'+ product.id +'" class="plus btn btn-info" style="margin-left: 5px">+</button>'
+                            +'</b><button data-art="'+ product.id +'" class="btn btn-delete btn-danger delete" style="position: absolute; right: -30px; top: -40px ">x</button>'
                         +'</div>'
                     +'</div>'
                 +'</div>'
@@ -53,3 +123,7 @@ function createDiv(product, cart) {
 }
 
 checkCart();
+
+function saveCartToLS(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
